1a9f6bb547ccab1b386753546b35ceae9da08ba0
kYasinAblay <58375357+kYasinAblay@users.noreply.github.com>
2019-12-23 22:59:07 +0300

    Duzeltme

--- K.YasinABLAY_Odev2/Form1.cs
+++ K.YasinABLAY_Odev2/Form1.cs
@@ -174,16 +174,15 @@ namespace K.YasinABLAY_Odev2
         {
             if (listSiparisDetayi.SelectedIndex == -1)
             {
-
                 return;
             }
-            else if (listSiparisDetayi.SelectedIndex > 0)
+            else if (listSiparisDetayi.SelectedItems.Count==1)
             {
                 listSiparisDetayi.Items.Remove(listSiparisDetayi.SelectedItem);
             }
             else
             {
-                if (MessageBox.Show("Siparişi silmek istiyormusunuz?", "Silme İşlemi", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                if (MessageBox.Show("Seçili siparişleri silmek mi istiyorsunuz?", "Silme İşlemi", MessageBoxButtons.YesNo,MessageBoxIcon.Information) == DialogResult.Yes)
                 {
                     while (listSiparisDetayi.SelectedItems.Count > 0)
                     {
@@ -194,114 +193,117 @@ namespace K.YasinABLAY_Odev2
         }
         private void btnDegistir_Click(object sender, EventArgs e)
         {
+            if (listSiparisDetayi.Items.Count > 0)
+            {
 
-            toplamTutar = 0;
-            if (listCesitler.SelectedIndex > 0)
-            {
-                cesit = listCesitler.SelectedItem.ToString();
-            }
-            else
-            {
-                cesit = listCesitler.Items[0].ToString();
-                listCesitler.SelectedIndex++;
-            }
-
-            if (cmbEbat.SelectedIndex == -1)
-            {
-                ebat = cmbEbat.Items[0].ToString();
-            }
-            else
-            {
-                ebat = cmbEbat.SelectedItem.ToString();
-            }
-            adet = Convert.ToInt32(nmricAdet.Value);
-            if (listCesitler.SelectedIndex == 0 || listCesitler.SelectedIndex == 1)
-            {
-                toplamTutar += 12;
-                if (cmbEbat.SelectedIndex == 1)
+                toplamTutar = 0;
+                if (listCesitler.SelectedIndex > 0)
                 {
-                    toplamTutar += 6;
+                    cesit = listCesitler.SelectedItem.ToString();
+                }
+                else
+                {
+                    cesit = listCesitler.Items[0].ToString();
+                    listCesitler.SelectedIndex++;
                 }
 
-                else if (cmbEbat.SelectedIndex == 0)
+                if (cmbEbat.SelectedIndex == -1)
                 {
-                    toplamTutar += 18;
+                    ebat = cmbEbat.Items[0].ToString();
+                }
+                else
+                {
+                    ebat = cmbEbat.SelectedItem.ToString();
+                }
+                adet = Convert.ToInt32(nmricAdet.Value);
+                if (listCesitler.SelectedIndex == 0 || listCesitler.SelectedIndex == 1)
+                {
+                    toplamTutar += 12;
+                    if (cmbEbat.SelectedIndex == 1)
+                    {
+                        toplamTutar += 6;
+                    }
+
+                    else if (cmbEbat.SelectedIndex == 0)
+                    {
+                        toplamTutar += 18;
+                    }
+
+                }
+                if (listCesitler.SelectedIndex == 2 || listCesitler.SelectedIndex == 3 || listCesitler.SelectedIndex == 4)
+                {
+                    toplamTutar += 14;
+                    if (cmbEbat.SelectedIndex == 1)
+                    {
+                        toplamTutar += 7;
+                    }
+
+                    else if (cmbEbat.SelectedIndex == 0)
+                    {
+                        toplamTutar += 21;
+                    }
+                }
+                if (listCesitler.SelectedIndex == 5 || listCesitler.SelectedIndex == 6)
+                {
+                    toplamTutar += 16;
+                    if (cmbEbat.SelectedIndex == 1)
+                    {
+                        toplamTutar += 8;
+                    }
+
+                    else if (cmbEbat.SelectedIndex == 0)
+                    {
+                        toplamTutar += 24;
+                    }
                 }
 
-            }
-            if (listCesitler.SelectedIndex == 2 || listCesitler.SelectedIndex == 3 || listCesitler.SelectedIndex == 4)
-            {
-                toplamTutar += 14;
-                if (cmbEbat.SelectedIndex == 1)
+                if (rbtninceKenar.Checked)
                 {
-                    toplamTutar += 7;
+                    toplamTutar += 2;
+                }
+                if (cBSalam.Checked)
+                {
+                    toplamTutar += 0.5M;
+                }
+                if (cBSosis.Checked)
+                {
+                    toplamTutar += 0.5M;
+                }
+                if (cBSucuk.Checked)
+                {
+                    toplamTutar += 0.5M;
+                }
+                if (cBJalepeno.Checked)
+                {
+                    toplamTutar += 0.5M;
+                }
+                if (cBAncuez.Checked)
+                {
+                    toplamTutar += 0.5M;
+                }
+                if (cBZeytin.Checked)
+                {
+                    toplamTutar += 0.5M;
                 }
 
-                else if (cmbEbat.SelectedIndex == 0)
+
+
+                if (listSiparisDetayi.SelectedIndex > 0)
                 {
-                    toplamTutar += 21;
+                    secilenIndis = listSiparisDetayi.SelectedIndex;
                 }
-            }
-            if (listCesitler.SelectedIndex == 5 || listCesitler.SelectedIndex == 6)
-            {
-                toplamTutar += 16;
-                if (cmbEbat.SelectedIndex == 1)
+                else
                 {
-                    toplamTutar += 8;
+                    secilenIndis = 0;
                 }
+                lblToplamTutar.Text = toplamTutar.ToString();
 
-                else if (cmbEbat.SelectedIndex == 0)
-                {
-                    toplamTutar += 24;
-                }
-            }
+                listSiparisDetayi.Items.RemoveAt(secilenIndis);
+                detay = adet + " Adet/" + ebat + " Boy " + cesit + "/" + toplamTutar * adet + " TL;";
+                listSiparisDetayi.Items.Insert(secilenIndis, detay);
 
-            if (rbtninceKenar.Checked)
-            {
-                toplamTutar += 2;
-            }
-            if (cBSalam.Checked)
-            {
-                toplamTutar += 0.5M;
-            }
-            if (cBSosis.Checked)
-            {
-                toplamTutar += 0.5M;
-            }
-            if (cBSucuk.Checked)
-            {
-                toplamTutar += 0.5M;
-            }
-            if (cBJalepeno.Checked)
-            {
-                toplamTutar += 0.5M;
-            }
-            if (cBAncuez.Checked)
-            {
-                toplamTutar += 0.5M;
-            }
-            if (cBZeytin.Checked)
-            {
-                toplamTutar += 0.5M;
-            }
-
-
-
-            if (listSiparisDetayi.SelectedIndex > 0)
-            {
-                secilenIndis = listSiparisDetayi.SelectedIndex;
-            }
-            else
-            {
                 secilenIndis = 0;
             }
-            lblToplamTutar.Text = toplamTutar.ToString();
-
-            listSiparisDetayi.Items.RemoveAt(secilenIndis);
-            detay = adet + " Adet/" + ebat + " Boy " + cesit + "/" + toplamTutar * adet + " TL;";
-            listSiparisDetayi.Items.Insert(secilenIndis, detay);
-
-            secilenIndis = 0;
         }
         private void btnSecilenOzelliktekiPizzayiEkle_Click(object sender, EventArgs e)
         {
